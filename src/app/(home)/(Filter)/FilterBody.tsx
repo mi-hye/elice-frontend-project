995b5b164a4ee8713a978814e90a59ac75769c1c
mi-hye <satge13@gmail.com>
2024-10-03 16:42:00 +0900

    feat: 미들웨어에 api요청 기능 구현

--- src/app/(home)/(Filter)/FilterBody.tsx
+++ src/app/(home)/(Filter)/FilterBody.tsx
@@ -1,18 +1,35 @@
 import Card from "@/app/components/Card";
 
-export default function FilterBody() {
+interface Course {
+	id: number;
+	title: string;
+	imgUrl: string;
+	logoUrl: string;
+	classType: number;
+	description: string;
+	enrollType: number;
+	isFree: boolean;
+}
+
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+async function getCourses() {
+	const res = await fetch(`${apiURL}/api/course`);
+	return res.json();
+}
+export default async function FilterBody() {
+	const { courseCount, courses } = await getCourses();
+	// console.log("courses:?? ", courses);
+
 	return (
 		<>
 			<div className="bg-red-200 w-full">
-				<div className="py-3 border-b-1 text-black font-bold text-xs">전체 4개</div>
+				<div className="py-3 border-b-1 text-black font-bold text-xs">전체 {courseCount}개</div>
 			</div>
 			<div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{courses.map((course: Course) => (
+					<Card key={course.id} course={course} />
+				))}
 			</div>
 		</>
 	);
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,3 +1,40 @@
-export default function Card() {
-	return <div className="h-[338px] rounded-lg bg-white mt-4">카드카드</div>;
+interface Course {
+	id: number;
+	title: string;
+	imgUrl: string | null;
+	logoUrl: string;
+	classType: number;
+	description: string;
+	enrollType: number;
+	isFree: boolean;
+}
+
+const classType = ["프로그래밍 기초"];
+
+export default function Card({ course }: { course: Course }) {
+	return (
+		<div className="h-[338px] rounded-lg bg-white mt-4 flex flex-col">
+			{course.imgUrl ? (
+				<img className="rounded-t-lg h-[146px]" src={course.imgUrl} alt={course.title}></img>
+			) : (
+				<div className="rounded-t-lg w-full h-[146px] bg-[#3A3A4C] layout-center">
+					<img className="h-[80%]" src={course.logoUrl} alt={course.title} />
+				</div>
+			)}
+			<div className="py-[18px] px-[18px] flex flex-col flex-1 justify-between">
+				<div>
+					<label className="text-[#524fa1] text-xs font-bold leading-[1.6]">
+						{classType[course.classType]}
+					</label>
+					<div className="text-black text-[16px] font-bold line-clamp-2 my-1">{course.title}</div>
+					<div className="w-full text-[14px] font-bold line-clamp-2 leading-[1.6]">
+						{course.description}
+					</div>
+				</div>
+				<div className="border-t text-[#00ab53] font-bold pt-2">
+					{course.enrollType === 4 ? "구독" : course.isFree ? "무료" : "유료"}
+				</div>
+			</div>
+		</div>
+	);
 }
