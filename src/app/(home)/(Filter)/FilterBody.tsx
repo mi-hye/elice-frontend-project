beca8aa7467953e8a56ae07944b30d96fd1ea8a3
mi-hye <satge13@gmail.com>
2024-10-03 10:02:20 +0900

    feat: 필터의 카드 UI 부분 구현

--- src/app/(home)/(Filter)/FilterBody.tsx
+++ src/app/(home)/(Filter)/FilterBody.tsx
@@ -1,3 +1,19 @@
+import Card from "@/app/components/Card";
+
 export default function FilterBody() {
-	return <div className="bg-red-200 w-full">바디</div>;
+	return (
+		<>
+			<div className="bg-red-200 w-full">
+				<div className="py-3 border-b-1 text-black font-bold text-xs">전체 4개</div>
+			</div>
+			<div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</>
+	);
 }
---
+++ src/app/components/Card.tsx
@@ -0,0 +1,3 @@
+export default function Card() {
+	return <div className="h-[338px] rounded-lg bg-white mt-4">카드카드</div>;
+}
