c9eb462d686b57f4685ddc640df8a0b21d831fc0
mi-hye <satge13@gmail.com>
2024-10-03 16:51:51 +0900

    desing: 색 변경

--- src/app/(home)/(Filter)/FilterBody.tsx
+++ src/app/(home)/(Filter)/FilterBody.tsx
@@ -19,12 +19,13 @@ async function getCourses() {
 }
 export default async function FilterBody() {
 	const { courseCount, courses } = await getCourses();
-	// console.log("courses:?? ", courses);
 
 	return (
 		<>
-			<div className="bg-red-200 w-full">
-				<div className="py-3 border-b-1 text-black font-bold text-xs">전체 {courseCount}개</div>
+			<div className="w-full my-1 h-full">
+				<div className="w-full py-3 border-b-2 border-gray-300 text-black font-bold text-xs">
+					전체 {courseCount}개
+				</div>
 			</div>
 			<div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{courses.map((course: Course) => (
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import SearchArea from "./SearchArea";
 export default async function HomePage() {
 	return (
 		<main className="w-screen h-screen">
-			<div className="container p-6 bg-sky-200">
+			<div className="container p-6">
 				<SearchArea />
 				<Filter />
 			</div>
