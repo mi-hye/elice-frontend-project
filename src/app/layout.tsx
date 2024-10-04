f222e5dd3b34d8b0723c45511a09a9a6032b43e9
mi-hye <satge13@gmail.com>
2024-10-04 20:05:26 +0900

    bug: nextjs에서 fontawesome 초기 로딩시 css 누락 문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
 	title: "엘리스 미니 프로젝트",
 	description: "프론트엔드 미니 프로젝트",
